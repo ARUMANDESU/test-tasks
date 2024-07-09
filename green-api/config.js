7c984aec317b8d6d1db902496893757964e80ae9
ARUMANDESU <zh.arumandes@gmail.com>
2024-07-09 16:39:31 +0500

    getSettings, getStateInstance

---
+++ green-api/config.js
@@ -0,0 +1,5 @@
+// if you what to take it from the environment variables, you can use webpack to bundle it in the build
+// but I will keep it simple for now
+const config = {
+    apiUrl: 'https://7103.api.greenapi.com'
+};
--- green-api/methods.js
+++ green-api/methods.js
@@ -1,9 +1,62 @@
 function getSettings() {
-    document.getElementById('output').innerText = JSON.stringify({ message: `getSettings clicked` }, null, 4);
+    const apiUrl = getAPIUrl()
+    const instanceId = getInstanceId()
+    const apiToken = getApiToken()
 
+    if (!instanceId || !apiToken) {
+        document.getElementById('output').innerText = JSON.stringify({ error: 'Please provide instanceId and apiToken' }, null, 4);
+        return
+    }
+
+    fetch(`${apiUrl}/waInstance${instanceId}/getSettings/${apiToken}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then(data => {
+            document.getElementById('output').innerText = JSON.stringify(data, null, 4)
+        })
+        .catch((error) => {
+            document.getElementById('output').innerText = JSON.stringify({ error: error.message }, null, 4)
+        })
 }
 
 function getStateInstance() {
+    const apiUrl = getAPIUrl()
+    const instanceId = getInstanceId()
+    const apiToken = getApiToken()
+
+    if (!instanceId || !apiToken) {
+        document.getElementById('output').innerText = JSON.stringify({ error: 'Please provide instanceId and apiToken' }, null, 4);
+        return
+    }
+
+    fetch(`${apiUrl}/waInstance${instanceId}/getStateInstance/${apiToken}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then(data => {
+            document.getElementById('output').innerText = JSON.stringify(data, null, 4)
+        })
+        .catch((error) => {
+            document.getElementById('output').innerText = JSON.stringify({ error: error.message }, null, 4)
+        })
+
     document.getElementById('output').innerText = JSON.stringify({ message: "getStateInstance clicked" }, null, 4);
 }
 
@@ -68,6 +121,10 @@ function getMessage () {
 
 function getPhoneNumberFile () {
     return document.getElementById('phoneNumberFile').value
+}
+
+function getAPIUrl () {
+    return config.apiUrl
 }
 
 window.onload = function () {
